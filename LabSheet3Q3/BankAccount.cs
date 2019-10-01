8331533164c5e0ef3b6037187deedb47c5e564b4
S00189006 <S00189006@LAB-B1041-19>
2019-10-01 15:01:08 +0100

    Lab Sheet 3 Question 3

---
+++ LabSheet3Q3/BankAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheet3Q3
+{
+    class BankAccount
+    {
+        //Using shorthand properties
+        public string AccountHolder { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+
+        public BankAccount(string accountNumber, string accountHolder, decimal balance)
+        {
+            AccountNumber = accountNumber;
+            AccountHolder = accountHolder;
+            Balance = balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if(Balance >= amount)
+            {
+                Balance -= amount;
+            }
+        }
+
+        public string GetAccountDetails()
+        {
+            return $"Account Number: {AccountNumber}" +
+                $"\nAccount Holder: {AccountHolder}" +
+                $"\nAccount Balance: {Balance:C}";
+        }
+    }
+}
